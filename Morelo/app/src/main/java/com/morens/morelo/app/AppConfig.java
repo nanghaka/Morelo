1b367309154922f22f9c0e84680af9b3baf49ffd
nanghaka <dndannang@gmail.com>
2015-05-02 12:11:50 +0300

    Confirmed  Login, Register and GPS

--- Morelo/app/src/main/java/com/morens/morelo/app/AppConfig.java
+++ Morelo/app/src/main/java/com/morens/morelo/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.morens.morelo.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://192.168.0.104/morelo/mobile/";
+	public static String URL_LOGIN = "http://planetweneed.org/morelo/mobile/";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.0.104/morelo/mobile//";
+	public static String URL_REGISTER = "http://planetweneed.org/morelo/mobile//";
 }
