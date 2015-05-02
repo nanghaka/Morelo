813f25b6449861ea340a98ede1b58dc136c5ac29
nanghaka <dndannang@gmail.com>
2015-05-02 18:18:16 +0300

    Add page to pick crimes for report
    This crimes are picked from the Database

--- Morelo/app/src/main/java/com/morens/morelo/ReportFormActivity.java
+++ Morelo/app/src/main/java/com/morens/morelo/ReportFormActivity.java
@@ -2,6 +2,7 @@ package com.morens.morelo;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,11 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,8 +29,8 @@ import java.util.List;
 
 public class ReportFormActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-    private Button btnAddNewCategory;
-    private TextView txtCategory;
+  //  private Button btnAddNewCategory;
+  //  private TextView txtCategory;
     private Spinner spinnerFood;
     // array list for spinner adapter
     private ArrayList<Category> categoriesList;
@@ -41,7 +39,8 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
     // API urls
 
     // Url to create new category
-    private String URL_NEW_CATEGORY = "http://10.0.2.2/morelo/new_category.php";
+    private String URL_NEW_CATEGORY = "http://planetweneed.org/morelo/mobile/new_category.php";
+
     // Url to get all categories
     private String URL_CATEGORIES = "http://planetweneed.org/morelo/mobile/get_categories.php";
 
@@ -50,9 +49,40 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reportform_layout);
 
-        //btnAddNewCategory = (Button) findViewById(R.id.btnAddNewCategory);
+
+        //This is for the GPS Location
+
+                String address = "";
+        GPSService mGPSService = new GPSService(ReportFormActivity.this);
+        mGPSService.getLocation();
+
+        if (mGPSService.isLocationAvailable == false) {
+
+            // Here you can ask the user to try again, using return; for that
+            Toast.makeText(getApplicationContext(), "Your location is not available, please try again.", Toast.LENGTH_SHORT).show();
+            return;
+
+            // Or you can continue without getting the location, remove the return; above and uncomment the line given below
+            // address = "Location not available";
+        } else {
+
+            // Getting location co-ordinates
+            double latitude = mGPSService.getLatitude();
+            double longitude = mGPSService.getLongitude();
+            Toast.makeText(getApplicationContext(), "Latitude:" + latitude + " | Longitude: " + longitude, Toast.LENGTH_LONG).show();
+
+            address = mGPSService.getLocationAddress();
+        }
+
+        Toast.makeText(getApplicationContext(), "Your address is: " + address, Toast.LENGTH_SHORT).show();
+
+// make sure you close the gps after using it. Save user's battery power
+        mGPSService.closeGPS();
+
+
+//        btnAddNewCategory = (Button) findViewById(R.id.btnAddNewCategory);
         spinnerFood = (Spinner) findViewById(R.id.spinCrimes);
-       // txtCategory = (TextView) findViewById(R.id.txtCategory);
+//        txtCategory = (TextView) findViewById(R.id.txtCategory);
 
         categoriesList = new ArrayList<Category>();
 
@@ -63,29 +93,30 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
         ReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent sendreport = new Intent(ReportFormActivity.this);
+                Intent finalintent = new Intent(ReportFormActivity.this, FinalActivity.class);
+                startActivity(finalintent);
             }
         });
 
         // Add new category click event
-        btnAddNewCategory.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (txtCategory.getText().toString().trim().length() > 0) {
-
-                    // new category name
-                    String newCategory = txtCategory.getText().toString();
-
-                    // Call Async task to create new category
-                    new AddNewCategory().execute(newCategory);
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "Please enter category name", Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-        });
+//        btnAddNewCategory.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                if (txtCategory.getText().toString().trim().length() > 0) {
+//
+//                    // new category name
+//                    String newCategory = txtCategory.getText().toString();
+//
+//                    // Call Async task to create new category
+//                    new AddNewCategory().execute(newCategory);
+//                } else {
+//                    Toast.makeText(getApplicationContext(),
+//                            "Please enter category name", Toast.LENGTH_SHORT)
+//                            .show();
+//                }
+//            }
+//        });
 
         new GetCategories().execute();
 
@@ -97,7 +128,7 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
     private void populateSpinner() {
         List<String> lables = new ArrayList<String>();
 
-        txtCategory.setText("");
+        //txtCategory.setText("");
 
         for (int i = 0; i < categoriesList.size(); i++) {
             lables.add(categoriesList.get(i).getName());
@@ -124,7 +155,7 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
         protected void onPreExecute() {
             super.onPreExecute();
             pDialog = new ProgressDialog(ReportFormActivity.this);
-            pDialog.setMessage("Fetching food categories..");
+            pDialog.setMessage("Fetching Criminal categories...");
             pDialog.setCancelable(false);
             pDialog.show();
 
@@ -176,75 +207,75 @@ public class ReportFormActivity extends Activity implements AdapterView.OnItemSe
     /**
      * Async task to create a new food category
      * */
-    private class AddNewCategory extends AsyncTask<String, Void, Void> {
-
-        boolean isNewCategoryCreated = false;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(ReportFormActivity.this);
-            pDialog.setMessage("Creating new category..");
-            pDialog.setCancelable(false);
-            pDialog.show();
-
-        }
-
-        @Override
-        protected Void doInBackground(String... arg) {
-
-            String newCategory = arg[0];
-
-            // Preparing post params
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("name", newCategory));
-
-            ServiceHandler serviceClient = new ServiceHandler();
-
-            String json = serviceClient.makeServiceCall(URL_NEW_CATEGORY,
-                    ServiceHandler.POST, params);
-
-            Log.d("Create Response: ", "> " + json);
-
-            if (json != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(json);
-                    boolean error = jsonObj.getBoolean("error");
-                    // checking for error node in json
-                    if (!error) {
-                        // new category created successfully
-                        isNewCategoryCreated = true;
-                    } else {
-                        Log.e("Create Category Error: ", "> " + jsonObj.getString("message"));
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            } else {
-                Log.e("JSON Data", "Didn't receive any data from server!");
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-            if (isNewCategoryCreated) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // fetching all categories
-                        new GetCategories().execute();
-                    }
-                });
-            }
-        }
-    }
+//    private class AddNewCategory extends AsyncTask<String, Void, Void> {
+//
+//        boolean isNewCategoryCreated = false;
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            pDialog = new ProgressDialog(ReportFormActivity.this);
+//            pDialog.setMessage("Creating new category..");
+//            pDialog.setCancelable(false);
+//            pDialog.show();
+//
+//        }
+//
+//        @Override
+//        protected Void doInBackground(String... arg) {
+//
+//            String newCategory = arg[0];
+//
+//            // Preparing post params
+//            List<NameValuePair> params = new ArrayList<NameValuePair>();
+//            params.add(new BasicNameValuePair("name", newCategory));
+//
+//            ServiceHandler serviceClient = new ServiceHandler();
+//
+//            String json = serviceClient.makeServiceCall(URL_NEW_CATEGORY,
+//                    ServiceHandler.POST, params);
+//
+//            Log.d("Create Response: ", "> " + json);
+//
+//            if (json != null) {
+//                try {
+//                    JSONObject jsonObj = new JSONObject(json);
+//                    boolean error = jsonObj.getBoolean("error");
+//                    // checking for error node in json
+//                    if (!error) {
+//                        // new category created successfully
+//                        isNewCategoryCreated = true;
+//                    } else {
+//                        Log.e("Create Category Error: ", "> " + jsonObj.getString("message"));
+//                    }
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            } else {
+//                Log.e("JSON Data", "Didn't receive any data from server!");
+//            }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void result) {
+//            super.onPostExecute(result);
+//            if (pDialog.isShowing())
+//                pDialog.dismiss();
+//            if (isNewCategoryCreated) {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // fetching all categories
+//                        new GetCategories().execute();
+//                    }
+//                });
+//            }
+//        }
+//    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position,
